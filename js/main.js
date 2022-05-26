874c96117d1d92c95895599a922c9411138774e8
david.xavier <Pi#314159>
2022-05-26 16:56:01 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+alert(d.getFullYear());
+
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert(idade + " anos, maior de idade.");
+}else{
+    alert(idade + " anos, menor de idade.");
+}
+*/
+
+/*
+var fruta = [{nome:"caju", cor:"amarela"}, {nome: "acerola", cor: "vermelha"}];
+console.log(fruta);
+alert(fruta[1].nome);
+*/
+
+/*
+var nome = "Roberto David Xavier";
+var n1 = 5;
+var n2 = 3;
+var frase = "Nigéria é melhor time do mundo!"
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase);
+console.log(frase.replace("Nigéria", "Camarões"));
+alert(frase.toUpperCase("Nigéria", "Congo"));
+*/
+
+/*
+var lista = ["maracujá", "acerola", "abacaxi"];
+lista.pop();
+lista.push("graviola");
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" | "));
+*/
