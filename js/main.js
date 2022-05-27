8211d9f589e65d821423dc663af2a846f2643ad3
david.xavier <Pi#314159>
2022-05-27 09:02:34 -0300

    JavaScript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+var validar = 0;
+function validaIdade(idade){
+    
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+var idade = prompt("Qual a sua idade?")
+validaIdade(idade);
+console.log(validar);
+
+
+
+
+
+
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+alert(soma(5, 10));
+*/
+
 /*
 var d = new Date();
 alert(d.getMonth()+1);
